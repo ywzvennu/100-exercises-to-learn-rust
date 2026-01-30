62eede8a0cf71639b37ee2c5fa0553bcbc9a15b0
ywzvennu <ywzvennu@gmail.com>
2026-01-30 21:06:23 +0300

    add 03_ticker_v1

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -10,22 +10,40 @@ pub struct Ticket {
 }
 
 impl Ticket {
-    pub fn new(title: String, description: String, status: String) -> Ticket {
+    fn validate_title(title: &String) -> bool {
         if title.is_empty() {
             panic!("Title cannot be empty");
         }
         if title.len() > 50 {
             panic!("Title cannot be longer than 50 bytes");
         }
+
+        true
+    }
+
+    fn validate_description(description: &String) -> bool {
         if description.is_empty() {
             panic!("Description cannot be empty");
         }
         if description.len() > 500 {
             panic!("Description cannot be longer than 500 bytes");
         }
+
+        true
+    }
+
+    fn validate_status(status: &String) -> bool {
         if status != "To-Do" && status != "In Progress" && status != "Done" {
             panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
         }
+
+        true
+    }
+
+    pub fn new(title: String, description: String, status: String) -> Ticket {
+        Self::validate_description(&description);
+        Self::validate_title(&title);
+        Self::validate_status(&status);
 
         Ticket {
             title,
@@ -44,6 +62,24 @@ impl Ticket {
 
     pub fn status(&self) -> &String {
         &self.status
+    }
+
+    pub fn set_description(&mut self, description: String) {
+        if Self::validate_description(&description) {
+            self.description = description;
+        }
+    }
+
+    pub fn set_title(&mut self, title: String) {
+        if Self::validate_title(&title) {
+            self.title = title;
+        }
+    }
+
+    pub fn set_status(&mut self, status: String) {
+        if Self::validate_status(&status) {
+            self.status = status;
+        }
     }
 }
 
--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,84 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order {
+    product_name: String,
+    quantity: u64,
+    unit_price: u64,
+}
+
+impl Order {
+    fn validate_product_name(product_name: &String) -> bool {
+        if product_name.is_empty() {
+            panic!("product name cannot be empty");
+        }
+
+        if product_name.len() > 300 {
+            panic!("product name length cannot be greater than 300 bytes");
+        }
+
+        true
+    }
+
+    fn validate_quantity(quantity: &u64) -> bool {
+        if *quantity == 0 {
+            panic!("quantity cannot be zero");
+        }
+
+        true
+    }
+
+    fn validate_unit_price(unit_price: &u64) -> bool {
+        if *unit_price == 0 {
+            panic!("unit price cannot be zero");
+        }
+
+        true
+    }
+
+    pub fn new(product_name: String, quantity: u64, unit_price: u64) -> Self {
+        Self::validate_product_name(&product_name);
+        Self::validate_quantity(&quantity);
+        Self::validate_unit_price(&unit_price);
+
+        Self {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u64 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u64 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        if Self::validate_product_name(&product_name) {
+            self.product_name = product_name;
+        }
+    }
+
+    pub fn set_quantity(&mut self, quantity: u64) {
+        if Self::validate_quantity(&quantity) {
+            self.quantity = quantity;
+        }
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u64) {
+        if Self::validate_unit_price(&unit_price) {
+            self.unit_price = unit_price;
+        }
+    }
+
+    pub fn total(&self) -> u64 {
+        self.quantity * self.unit_price
+    }
+}
